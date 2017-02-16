fee0cdc2110dec24f5cf6fea3664c6d3bb4845a5
RajeshAdloori <rajesh@huskers.unl.edu>
2017-02-15 23:25:55 -0600

    Adding ng touchable highlights to the feed items

--- Feed.js
+++ Feed.js
@@ -51,31 +51,37 @@ class Feed extends Component {
 		})
 	}
 
+  pressRow(rowData) {
+    console.log(rowData.actor.login)
+  }
+
 	renderRow(rowData) {
-		console.log(rowData)
-		return <View style={{
-			flex: 1,
-			flexDirection: 'row',
-			padding: 20,
-			alignItems: 'center',
-			borderColor: '#D7D7D7',
-			borderBottomWidth: 1
-		}}>
-			<Image
-				source={{uri: rowData.actor.avatar_url}}
-				style={{
-					height: 36,
-					width: 36,
-					borderRadius: 18
-				}}/>
-				<View style={{paddingLeft: 20}}>
-					<Text>{moment(rowData.created_at).fromNow()}</Text>
-					<Text>{rowData.actor.login}</Text>
-					<Text>{rowData.repo.name}</Text>
-					<Text style={{fontWeight: 'bold'}}>{rowData.payload.action}</Text>
-				</View>
-		</View>
-	}
+		return (
+      <TouchableHighlight onPress={() => this.pressRow(rowData)} underlayColor='#ddd'>
+        <View style={{
+    			flex: 1,
+    			flexDirection: 'row',
+    			padding: 20,
+    			alignItems: 'center',
+    			borderColor: '#D7D7D7',
+    			borderBottomWidth: 1
+    		}}>
+  			<Image
+  				source={{uri: rowData.actor.avatar_url}}
+  				style={{
+  					height: 36,
+  					width: 36,
+  					borderRadius: 18
+  				}}/>
+  				<View style={{paddingLeft: 20}}>
+  					<Text>{moment(rowData.created_at).fromNow()}</Text>
+  					<Text>{rowData.actor.login}</Text>
+  					<Text>{rowData.repo.name}</Text>
+  					<Text style={{fontWeight: 'bold'}}>{rowData.payload.action}</Text>
+  				</View>
+  		</View>
+    </TouchableHighlight>
+	)}
 
   render() {
 		if(this.state.showProgress) {
@@ -107,7 +113,54 @@ const styles = StyleSheet.create({
 	listRow: {
 		color: '#333',
 		alignSelf: 'center'
-	}
+	},
+	logo: {
+		width: 66,
+		height: 55
+	},
+	heading: {
+		fontSize: 30,
+		marginTop: 10,
+    height: 30,
+    marginBottom: 20
+	},
+	input: {
+		height: 50,
+		padding: 4,
+		marginTop: 10,
+		fontSize: 18,
+		borderWidth: 1,
+		borderColor: '#48BBEC'
+	},
+	LogoutButton: {
+		height: 100,
+		backgroundColor: '#48BBEC',
+		alignSelf: 'stretch',
+		marginTop: 10,
+		justifyContent: 'center',
+    marginBottom:50
+	},
+	buttonText: {
+		fontSize: 22,
+		color: '#FFF',
+		alignSelf: 'center'
+	},
+  loader: {
+    marginTop: 30
+  },
+  baseText: {
+    fontFamily: 'Cochin',
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  error: {
+    color: 'red',
+    marginTop: 10,
+    fontSize: 10,
+    height: 10
+  }
 })
 
 module.exports = Feed
